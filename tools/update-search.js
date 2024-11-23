7adf261b8e7b0000e4f69d62eb36b781ebca2b7c
stefanicjuraj <jurajstefanic@outlook.com>
2024-11-24 00:54:03 +0100

    chore: modify path search.json output path
    Signed-off-by: stefanicjuraj <jurajstefanic@outlook.com>

--- tools/update-search.js
+++ tools/update-search.js
@@ -137,7 +137,7 @@ function searchDocs() {
 function main() {
   const docsData = searchDocs()
   fs.writeFileSync(
-    path.join(__dirname, '../src/assets/search.json'),
+    path.join(__dirname, '../public/search.json'),
     JSON.stringify(docsData, null, 2),
     'utf8'
   )
