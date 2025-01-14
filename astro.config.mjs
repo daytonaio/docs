2be29e9e223407e9875481158678b42a17b85763
stefanicjuraj <jurajstefanic@outlook.com>
2025-01-14 22:30:39 +0100

    docs: fix sidebar config
    Signed-off-by: stefanicjuraj <jurajstefanic@outlook.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -132,6 +132,7 @@ export default defineConfig({
                 icon: 'server.svg',
               },
             },
+            {
               label: 'API Keys',
               link: '/configuration/api-keys',
               attrs: {
